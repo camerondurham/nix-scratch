8fdfabf139561303bc46f099d21c5ea5631b8427
Cam <17013462+camerondurham@users.noreply.github.com>
2023-04-20 21:33:43 -0700

    Create flake.nix

---
+++ ec2-ami-tests/flake.nix
@@ -0,0 +1,9 @@
+{
+
+# TODO: this example tutorial
+# http://jackkelly.name/blog/archives/2020/08/30/building_and_importing_nixos_amis_on_ec2/
+
+# used to build this repo
+# https://github.com/nh2/nixos-ami-building
+
+}
